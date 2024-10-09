71f1f77597609a999a1f9c59d5adc8ea82dd71d6
waldirborbajr <wborbajr@gmail.com>
2024-10-09 22:21:55 +0000

    wip

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -61,106 +61,16 @@
         "editor"
         "history"
         "directory"
-        "spectrum"
-        # "fasd"
-        "spectrum"
-        "utility"
         "ssh"
         "completion"
         "git"
-        "osx"
         "syntax-highlighting"
         "history-substring-search"
         "node"
-        # "homebrew"
-        # "haskell"
-        # "ruby"
         "prompt"
       ];
 
     };
-
-    # oh-my-zsh = {
-    #   enable = true;
-    #   theme = "lambda";
-    #   plugins = [ "sudo" "common-aliases" "mosh" "ssh-agent" "git" "fzf"  ];
-    #   extraConfig = ''
-    #     zstyle :omz:plugins:ssh-agent agent-forwarding yes
-    #     zstyle :omz:plugins:ssh-agent lazy yes
-    #   '';
-    # };
-
-    # plugins = [
-    #   {
-    #     name = "catppuccin-zsh-syntax-hightlighting";
-    #     src = pkgs.fetchFromGitHub {
-    #       owner = "catppuccin";
-    #       repo = "zsh-syntax-highlighting";
-    #       rev = "06d519c20798f0ebe275fc3a8101841faaeee8ea";
-    #       sha256 = "1yj916klvzpvwghii7m6qx2ya3p2kx25nydymilvjzbx8z0sdcj3";
-    #     };
-    #     file = "themes/catppuccin_mocha-zsh-syntax-highlighting.zsh";
-    #   }
-    #   {
-    #     name = "zsh-nix-shell";
-    #     file = "nix-shell.plugin.zsh";
-    #     src = pkgs.fetchFromGitHub {
-    #       owner = "chisui";
-    #       repo = "zsh-nix-shell";
-    #       rev = "v0.5.0";
-    #       sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
-    #     };
-    #   }
-    #   {
-    #     name = "fzf-tab";
-    #     src = pkgs.zsh-fzf-tab;
-    #     file = "share/fzf-tab/fzf-tab.zsh";
-    #   }
-    #   {
-    #     name = "zsh-syntax-highlighting";
-    #     file = "share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh";
-    #     src = pkgs.zsh-syntax-highlighting;
-    #   }
-    # ];
-
-
-    # initExtra = ''
-    #   zstyle ':completion:*' matcher-list "" 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-    #   # setopt MENU_COMPLETE
-    #   ulimit -n 4096
-    #
-    #   # fzf
-    #   export FZF_COMPLETION_TRIGGER='*'
-    #   export FZF_DEFAULT_COMMAND='fd --type file --follow --hidden --exclude .git'
-    #   export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-    #   _fzf_compgen_path() {
-    #     fd --hidden --follow --exclude '.git' . "$1"
-    #   }
-    #   _fzf_compgen_dir() {
-    #     fd --type d --hidden --follow --exclude '.git' . "$1"
-    #   }
-    #
-    #   # pure-prompt
-    #   . ${pkgs.pure-prompt}/share/zsh/site-functions/async
-    #   . ${pkgs.pure-prompt}/share/zsh/site-functions/prompt_pure_setup
-    #   PURE_GIT_PULL=0
-    #   RPS1=""
-    #
-    #   # zsh-fast-syntax-highlighting
-    #   . ${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh
-    #
-    #   # zsh-history-substring-search
-    #   . ${pkgs.zsh-history-substring-search}/share/zsh-history-substring-search/zsh-history-substring-search.zsh
-    #   bindkey '^[OA' history-substring-search-up
-    #   bindkey '^[OB' history-substring-search-down
-    #   bindkey -M emacs '^P' history-substring-search-up
-    #   bindkey -M emacs '^N' history-substring-search-down
-    #   bindkey -M vicmd 'k' history-substring-search-up
-    #   bindkey -M vicmd 'j' history-substring-search-down
-    #
-    #   # zsh-nix-shell
-    #   . ${pkgs.zsh-nix-shell}/share/zsh-nix-shell/nix-shell.plugin.zsh
-    # '';
   };
 
   programs.direnv = {
