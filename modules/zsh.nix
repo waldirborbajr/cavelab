00469adb26f38acfc6cd1da159f5041aa7f320c8
Waldir Borba Juniorm <wborbajr@gmail.com>
2024-10-14 22:07:05 +0000

    WIP

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -262,6 +262,21 @@ in
     # These will git cloned and sourced, so there is little overhead.
     plugins = with pkgs; [
       {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.8.0";
+          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+        };
+      }
+      {
+        name = "vi-mode";
+        src = pkgs.zsh-vi-mode;
+        file = "share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
+      }
+      {
         name = "zsh-autosuggestions";
         src = pkgs.fetchFromGitHub {
           owner = "zsh-users";
