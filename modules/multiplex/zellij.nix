0df0a4970253b0daadcc28aaaadd3f4dcd956650
Waldir Borba Junior <wborbajr@gmail.com>
2024-10-16 16:24:35 -0300

    WIP

--- modules/multiplex/zellij.nix
+++ modules/multiplex/zellij.nix
@@ -6,22 +6,39 @@
       enable = true;
       enableZshIntegration = true;
       settings = {
-        theme = "gruvbox-dark";
+        theme = "kanagawa";
         themes = {
-          gruvbox-dark = {
-            fg = "#D5C4A1";
-            bg = "#282828";
-            black = "#3C3836";
-            red = "#CC241D";
-            green = "#98971A";
-            yellow = "#D79921";
-            blue = "#3C8588";
-            magenta = "#B16286";
-            cyan = "#689D6A";
-            white = "#FBF1C7";
-            orange = "#D65D0E";
+          kanagawa = {
+            fg = "#DCD7BA";
+            bg = "#1F1F28";
+            red = "#FFA066"; # "#C34043";
+            green = "#76946A";
+            yellow = "#FF9E3B";
+            blue = "#7E9CD8";
+            magenta = "#957FB8";
+            orange =
+              "#16161D"; # original orange "#FFA066" but makes compact bar unreadable
+            cyan = "#7FB4CA";
+            black = "#16161D";
+            white = "#DCD7BA";
           };
         };
+        # theme = "gruvbox-dark";
+        # themes = {
+        #   gruvbox-dark = {
+        #     fg = "#D5C4A1";
+        #     bg = "#282828";
+        #     black = "#3C3836";
+        #     red = "#CC241D";
+        #     green = "#98971A";
+        #     yellow = "#D79921";
+        #     blue = "#3C8588";
+        #     magenta = "#B16286";
+        #     cyan = "#689D6A";
+        #     white = "#FBF1C7";
+        #     orange = "#D65D0E";
+        #   };
+        # };
       };
     };
   };
