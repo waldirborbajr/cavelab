76cb8ce0cec6f8f1e9d6d19a1536d45beb2edddd
Waldir Borba Juniorm <wborbajr@gmail.com>
2024-10-17 01:17:04 +0000

    WIP

--- home.nix
+++ home.nix
@@ -12,14 +12,8 @@
 
   # Home Manager needs a bit of information about you and the paths it should
   # manage.V
-  # home.username = "borba";
-  # home.homeDirectory = "/home/ubuntu";
-
-  home.username = "${user}";
-  home.homeDirectory = "/home/${user}";
-
-  # home.username = builtins.getEnv "USER";
-  # home.homeDirectory = builtins.getEnv "HOME";
+  home.username = "ubuntu";
+  home.homeDirectory = "/home/ubuntu";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
