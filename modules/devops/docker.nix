91f85a69a4baa2366bd1aeea601b36f6d2e0e05c
Waldir Borba Junior <wborbajr@gmail.com>
2024-11-05 18:04:29 -0300

    disable: docker

--- modules/devops/docker.nix
+++ modules/devops/docker.nix
@@ -5,10 +5,11 @@
   home = {
     packages = with pkgs; [
       lazydocker
-      docker
-      docker-compose
-      docker-credential-gcr
-      docker-credential-helpers
+      # docker
+      docker-client
+      # docker-compose
+      # docker-credential-gcr
+      # docker-credential-helpers
     ];
   };
 
