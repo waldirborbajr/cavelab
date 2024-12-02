99eefdbdc6fea8fff240598dc291bda0f3f60f1a
Waldir Borba Junior <wborbajr@gmail.com>
2024-12-02 17:26:40 -0300

    fix: https://github.com/nix-community/home-manager/issues/6160

--- home.nix
+++ home.nix
@@ -68,7 +68,8 @@
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
     # # fonts?
-    (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; })
+    # (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; })
+    pkgs.nerd-fonts.fira-code
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
