f846406bcb8b9f917d2c7c96abca1ca00f42cebb
Waldir Borba Junior <wborbajr@gmail.com>
2025-05-26 16:20:04 -0300

    zsh

--- modules/shell/yazi.nix
+++ modules/shell/yazi.nix
@@ -1,6 +1,22 @@
-{
+# {
+#   programs.yazi = {
+#     enable = true;
+#     enableZshIntegration = true;
+#   };
+# }
+
+{ pkgs, ... }: {
   programs.yazi = {
     enable = true;
+    package = pkgs.yazi;
     enableZshIntegration = true;
+    settings = {
+      manager = {
+        show_hidden = true;
+        sort_dir_first = true;
+        linemode = "owner";
+      };
+
+    };
   };
 }
