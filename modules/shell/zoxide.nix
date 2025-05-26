0d0dfce542e991b18ebd3c2d98aafc9db70e8af2
Waldir Borba Junior <wborbajr@gmail.com>
2025-05-26 15:39:19 -0300

    zoxide

--- modules/shell/zoxide.nix
+++ modules/shell/zoxide.nix
@@ -1,19 +1,29 @@
+# {
+#   lib,
+#   config,
+#   ...
+# }:
+# {
+#   options = {
+#     zoxide.enable = lib.mkEnableOption "Enable zoxide module";
+#   };
+#   config = lib.mkIf config.zoxide.enable {
+#     programs.zoxide = {
+#       enable = true;
+#       enableZshIntegration = true;
+#       options = [
+#         "--cmd cd"
+#       ];
+#     };
+#   };
+# }
+
 {
-  lib,
-  config,
-  ...
-}:
-{
-  options = {
-    zoxide.enable = lib.mkEnableOption "Enable zoxide module";
-  };
-  config = lib.mkIf config.zoxide.enable {
-    programs.zoxide = {
-      enable = true;
-      enableZshIntegration = true;
-      options = [
-        "--cmd cd"
-      ];
-    };
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+    options = [
+      "--cmd cd"
+    ];
   };
 }
