d7b89053116a9d5b107d81d7cefcea54e1f5b82d
Waldir Borba Junior <wborbajr@gmail.com>
2025-05-26 15:37:50 -0300

    http

---
+++ modules/tools/http.nix
@@ -0,0 +1,17 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  options = {
+    http.enable = lib.mkEnableOption "Enable http module";
+  };
+  config = lib.mkIf config.http.enable {
+    home.packages = with pkgs; [
+      curl
+      openssl
+    ];
+  };
+}
