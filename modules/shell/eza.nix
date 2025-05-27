3d413fba272fa70b9ca918afcc0f848b2e3768c7
Waldir Borba Junior <wborbajr@gmail.com>
2025-05-27 10:18:56 -0300

    eza

--- modules/shell/eza.nix
+++ modules/shell/eza.nix
@@ -1,10 +1,29 @@
+# {
+#   programs.eza = {
+#     enable = true;
+#     enableZshIntegration = true;
+#     extraOptions = ["--group-directories-first" "--header"];
+#     colors = "always";
+#     icons = "always";
+#     git = true;
+#   };
+# }
+
 {
   programs.eza = {
-    enable = true;
-    enableZshIntegration = true;
-    extraOptions = ["--group-directories-first" "--header"];
-    colors = "always";
-    icons = "always";
-    git = true;
+    icons = "auto";
+
+    enableNushellIntegration = false;
+
+    extraOptions = [
+      "-l"
+      "-a"
+      "--group"
+      "--group-directories-first"
+      "--no-user"
+      "--no-time"
+      "--no-permissions"
+      "--octal-permissions"
+    ];
   };
 }
