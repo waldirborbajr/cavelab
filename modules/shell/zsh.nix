02f0d710f038b0e7c1ae5e6a56d8aace85a29b73
Waldir Borba Junior <wborbajr@gmail.com>
2025-05-27 14:18:54 -0300

    zsh

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -68,14 +68,12 @@
       _comp_options+=(globdots)
     '';
 
-
-    initExtra = '''';
- 
-    initContent = ''
-
+    initExtra = ''
       # which starship >/dev/null && eval "$(starship init zsh)"
       which zoxide >/dev/null && eval "$(zoxide init zsh --cmd cd)"
-
+    '';
+ 
+    initContent = ''
       while read -r option; do
         setopt $option
       done <<-EOF
